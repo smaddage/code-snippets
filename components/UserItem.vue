46ba2523df30e5150d7900181dbc1f0516e9eac9
Savinda Maddage <maddsa@uptrends.com>
2026-01-21 12:13:44 +0100

    update components

---
+++ components/UserItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div @click="selectUser">
+      <p>{{ user.name }}</p>
+      <p>{{ user.email }}</p>
+    </div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+    user: Object
+  });
+  
+  const emit = defineEmits(["select"]);
+  
+  function selectUser() {
+    emit("select", props.user);
+  }
+  </script>
